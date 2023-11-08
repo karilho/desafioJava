79bd89c25fa97e6ade5ba3bfed919c7f507d523c
lucalmeida <lucalmeida@brasilseg.com.br>
2023-11-08 15:56:22 -0300

    DESAFIO PASSO 1 MELHORADO!

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import services.Leitores;
 
 import java.io.IOException;
+import java.util.List;
 
 public class main {
     // Throws io EXCEPTION - > SIGNIFICA QUE ELE VAI LANÇAR ERRO IN OUT CASO DÊ MERDA.
@@ -9,6 +10,11 @@ public class main {
         Leitores leitorInstanciado = new Leitores();
 
         // CHAMAR O METODO LEITORDETXT DO NOSSO LEITOR INSTANCIADO.
-        leitorInstanciado.leitorDeTXT();
+        List<String> perguntasDoTXT = leitorInstanciado.leitorDeTXT();
+
+        for (String pergunta : perguntasDoTXT){
+            System.out.println(pergunta);
+        }
+
     }
 }
--- services/Leitores.java
+++ services/Leitores.java
@@ -4,18 +4,28 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Leitores {
-    public void leitorDeTXT () throws IOException {
-        // Leia o arquivo.txt
-        BufferedReader leitorDePerguntas = new BufferedReader(new FileReader("formulario.txt"));
-        String linhaASerLida;
-        // enquanto houverem linhas, leia as linhas.
+    public List<String> leitorDeTXT() throws IOException {
+        List<String> listaComTodasPerguntas = new ArrayList<>();
 
-        while ((linhaASerLida = leitorDePerguntas.readLine()) != null ) {
-            // Depois print na tela cada linha.
-            System.out.println(linhaASerLida);
+        try {
+            BufferedReader leitorDePerguntas = new BufferedReader(new FileReader("formulario.txt"));
+
+            // Leia o arquivo.txt
+            String linhaASerLida;
+            // enquanto houverem linhas, leia as linhas.
+            while ((linhaASerLida = leitorDePerguntas.readLine()) != null ) {
+                //Adicione as perguntas dentro do array
+                listaComTodasPerguntas.add(linhaASerLida);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+
+        return listaComTodasPerguntas;
 
     }
 }
